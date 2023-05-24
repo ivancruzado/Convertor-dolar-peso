a0fe4ba8352c55ca53ba2ce5ae906ac2835f12a3
Ivan <ivanc2814@gmail.com>
2023-05-24 04:03:34 -0300

    version 1.0

---
+++ ConversorMoneda/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+//practica encapsulamiento y modificacion
+
+namespace ConversorMoneda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversor obj = new Conversor();
+
+            obj.CambiarCotizacionPeso(433);
+
+            Console.WriteLine("escriba cantidad de dolares a convertir: ");
+            double dolares = double.Parse(Console.ReadLine());
+            double conversion = obj.Convertir(dolares);
+            Console.WriteLine($"${dolares} dolares son ${conversion} pesos");
+
+        }
+    }
+
+    class Conversor
+    {
+        private double pesoTarjeta = 430;
+
+        public double Convertir(double dolares)
+        {
+            return dolares * pesoTarjeta;
+        }
+
+        public void CambiarCotizacionPeso(double nuevoPeso)
+        {
+            if (nuevoPeso < 0){
+                pesoTarjeta = 420;
+            }
+            else
+            {
+                pesoTarjeta = nuevoPeso;
+            }
+            
+        }
+    }
+}
